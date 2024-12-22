20c0cc33924029c45e91d4e98451d45c66332e02
Kostiantyn <sinkkostia@gmail.com>
2024-12-22 02:38:37 +0200

    Refactoring

--- app/urls.py
+++ app/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),  # Головна сторінка
-    path('cluster/', views.cluster, name='cluster'),  # Кластеризація
-    path('info/', views.info, name='info'),  # Інформаційна сторінка
-    path('teach/', views.teach, name='teach'),  # Навчальна сторінка
+    path('', views.index, name='index'),
+    path('cluster/', views.cluster, name='cluster'),
+    path('info/', views.info, name='info'),
+    path('teach/', views.teach, name='teach'),
 ]
